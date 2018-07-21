6c8114b5eac3771d0deebeebea294fc1b084bed2
Chad Spray <cspray@fanthreesixty.com>
2018-07-21 14:12:44 -0500

    Renames the splash screen fragment

--- app/src/main/java/com/groupify/groupify/SplashScreenFragment.java
+++ app/src/main/java/com/groupify/groupify/SplashScreenFragment.java
@@ -8,7 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class SplashScreen extends Fragment {
+public class SplashScreenFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
