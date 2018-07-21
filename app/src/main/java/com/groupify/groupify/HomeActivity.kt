bf435dc08dd07ffc85fccd0a9ac88b89e655da42
Nicholas McClain-Simpson <nmcclainsimpson@fanthreesixty.com>
2018-07-21 16:58:46 -0500

    Change app colors

--- app/src/main/java/com/groupify/groupify/HomeActivity.kt
+++ app/src/main/java/com/groupify/groupify/HomeActivity.kt
@@ -3,6 +3,7 @@ package com.groupify.groupify
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 
 class HomeActivity : AppCompatActivity() {
 
