c9186547e04652f4b7cb9c49b25db49709c97159
Nicholas McClain-Simpson <nmcclainsimpson@fanthreesixty.com>
2018-07-21 14:06:17 -0500

    Reroute start to splash

---
+++ app/src/main/java/com/groupify/groupify/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.groupify.groupify
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
