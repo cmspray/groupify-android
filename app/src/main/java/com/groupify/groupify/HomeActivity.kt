3e37605a32d45525cf115f28cfbf43a2dd7dcd42
Nicholas McClain-Simpson <nmcclainsimpson@fanthreesixty.com>
2018-07-21 16:03:57 -0500

    add menu bar

--- app/src/main/java/com/groupify/groupify/HomeActivity.kt
+++ app/src/main/java/com/groupify/groupify/HomeActivity.kt
@@ -2,11 +2,23 @@ package com.groupify.groupify
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+
+
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.android, menu)
+        return true
     }
 }
