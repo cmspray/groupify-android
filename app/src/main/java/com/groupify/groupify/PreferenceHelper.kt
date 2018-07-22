693fc45d66f309c92ba5ac3e873280f68e329f06
Chad Spray <cspray@fanthreesixty.com>
2018-07-21 22:42:24 -0500

    Adds onBoardingPref

--- app/src/main/java/com/groupify/groupify/PreferenceHelper.kt
+++ app/src/main/java/com/groupify/groupify/PreferenceHelper.kt
@@ -7,9 +7,14 @@ class PreferenceHelper {
 
     companion object {
         fun getDefaultPreferences(context: Context) = PreferenceManager.getDefaultSharedPreferences(context)
+        //UserId
         fun getUserId(context: Context) = getDefaultPreferences(context).getString(context.getString(R.string.spotify_user_id_pref), "")
+        fun saveUserId(context: Context, userId: String) = getDefaultPreferences(context).edit().putString(context.getString(R.string.spotify_user_id_pref), userId).apply()
+        //Auth
         fun getAuthToken(context: Context) = getDefaultPreferences(context).getString(context.getString(R.string.spotify_token_pref), "")
         fun getAuth(context: Context) = String.format("Bearer %s", getAuthToken(context))
-        fun saveUserId(context: Context, userId: String) = getDefaultPreferences(context).edit().putString(context.getString(R.string.spotify_user_id_pref), userId).apply()
+        //OnBoarding
+        fun setHasSeenOnBoarding(context: Context) = getDefaultPreferences(context).edit().putBoolean(context.getString(R.string.spotify_onboard_pref), true).apply()
+        fun hasSeenOnBoarding(context: Context) = getDefaultPreferences(context).getBoolean(context.getString(R.string.spotify_onboard_pref), false)
     }
 }
